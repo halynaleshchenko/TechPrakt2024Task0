1f0144d53a6a650774d5d684e55c917d0f245682
Galina Leshchenko <ipz222_lgo@student.ztu.edu.ua>
2024-02-10 20:13:02 +0200

    +.gitignore and +ConsoleSumLeshchenko

---
+++ ConsoleSumLeshchenko/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleSumLeshchenko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Перший доданок:");          
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Другий доданок:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int sum = number1 + number2;
+            Console.WriteLine($"Ваш вираз: {number1} + {number2} = {sum}");          
+        }
+    }
+}
